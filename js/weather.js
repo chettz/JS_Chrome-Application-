17c425bff7cd46f65d08a088f32a58a012de9c0d
chettz <chyohan85@gmail.com>
2023-07-28 01:39:15 +0900

    add weather api

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "5f03516ba2c52fb01205be5093415e58";
+
 function onGeoOk(position) {
    const lat = position.coords.latitude;
    const lon = position.coords.longitude;
-   console.log("You live in", lat, lon);
+   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+   fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+         const weather = document.querySelector("#weather span:first-child");
+         const city = document.querySelector("#weather span:last-child");
+         city.innerText = data.name;
+         weather.innerText = `${data.weather[0].main} / ${data.main.temp}도`;
+      });
 }
 
 function onGeoError() {
