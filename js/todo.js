ace26c7438c8595c9a66f5cfb204d50ef58146e8
chettz <chyohan85@gmail.com>
2023-07-27 16:01:30 +0900

    add local storage

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,15 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+   localStorage.setItem("todos", JSON.stringify(toDos));
+   // object나 array등을 string으로 바꾸어줌 -> 배열에 이미 문자열로 저장되어 있는데 왜쓸까?
+   //localstorage는 배열을 저장하지 못함, 배열 변수를 넘겨주어도 [a,b,c,]로 저장되는게 아닌 a,b,c로 저장됨
+   //localstorage는 문자열만 저장 가능 -> stringfy를 이용하여 "["a", "b", "c"]" 배열 모양의 문자열로 저장
+}
+
 function deleteToDo(event) {
    const li = event.target.parentElement;
    li.remove();
@@ -23,7 +32,9 @@ function handleToDoSubmit(event) {
    event.preventDefault();
    const newTodo = toDoInput.value;
    toDoInput.value = ""; //input 박스를 비우기 위해
+   toDos.push(newTodo);
    paintToDo(newTodo);
+   saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
